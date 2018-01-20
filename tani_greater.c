df94c28f561a89c993341d7dc71f543bb142d7ec
JyotiVarshney02 <35510501+JyotiVarshney02@users.noreply.github.com>
2018-01-20 18:58:42 +0530

    Create tani_greater.c

---
+++ tani_greater.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int num1,num2,num3;
+	printf("Enter your numbers \n");
+	scanf("%d\n%d\n%d",&num1,&num2,&num3);
+	if(num1>num2&&num1>num3)
+	{
+		printf("%d is greater",num1);
+	}
+    else	
+	   if(num2>num1&&num2>num3)
+	{
+		printf("%d is greater",num2);
+	}
+	else
+	if(num3>num2&&num3>num1)
+	{
+		printf("%d is greater",num3);
+	}
+}
